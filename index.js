f073bc075d5fb5e258e02b80539c15351444d592
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-12-27 13:49:07 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+// console.log(dbpw.mongodbpw);
+
+mongoose.connect('mongodb://localhost:27017/myplug', {})
+
+  .then(() => console.log('MongoDB Connected!!'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
